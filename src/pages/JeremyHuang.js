e9a5fbb9a331fac33e80672f41da0bc773b5aa7d
Jeremy Huang <jeremylossi2016@gmail.com>
2023-11-09 15:12:08 -0700

    asdf

---
+++ src/pages/JeremyHuang.js
@@ -0,0 +1,125 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hey, I'm Jeremy. </h1>
+          </div>
+          <img
+            className='pic'
+            src='https://i.postimg.cc/qR2VgDkd/spiderman.jpg'
+            alt='tom holland'
+          />
+
+          <p className='description'>
+            Hi, I'm Jeremy Huang, a freshman studying Math and CS at CU Boulder.
+            I am a depressed Broncos fan (Why, Russel Wilson, why can't you throw
+            a ball????). I am also quite the Nuggets fan and watch too much basketball
+            for my own good. I should probably spend all this time actually studying Math
+            and CS rather than watching Nikola Jokic be too good at basketball. 
+            I also play a lot of Catan - especailly during COVID lockdown. If you 
+            ever want to play a game of Catan online (colonist.io), DM me on discord
+            at "sandsowkd".
+          </p>
+
+          <a className='button' href='https://www.linkedin.com/in/jerhuang/'>
+            <p> My Linkedin </p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
