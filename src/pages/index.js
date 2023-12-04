840abbca606d44b3f7ed2dcc61d0dde4e1141f57
Kevin <kevinshu.colorado@gmail.com>
2023-12-03 23:24:08 -0700

    Changed Initial Page To Link To Mine

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,11 @@ export default function Home() {
         <code>src/page/teamMemberName.js</code>
 
         <div className='grid'>
+          <a href='/kevinshu ' className='card'>
+            <h3>Kevin's Page &rarr;</h3>
+            <p>Click here to Cat</p>
+          </a>
+
           <a href='/TomHolland ' className='card'>
             <h3>Example About Me Page &rarr;</h3>
             <p>Click here to navigate to an example page</p>
